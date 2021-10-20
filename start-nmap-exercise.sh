a0ea8fe3ad3ae64e5447b135872102263a5efb90
rfifarek <rfifarek@gmail.com>
2021-10-19 21:10:21 -0700

    Update start-nmap-exercise.sh

--- start-nmap-exercise.sh
+++ start-nmap-exercise.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+if [ "$EUID" -ne 0 ]
+  then echo "Error: Please run with sudo"
+  exit
+fi
+
 for port in 22 23 53 80 443 8080 3306 5432 7802 12345 
 do
 	ncat -kl $port 2>/dev/null &
