bd3fc75171bab3272731a975a4ba2233bada4106
rfifarek <rfifarek@gmail.com>
2021-10-21 22:49:57 -0700

    Update start-nmap-exercise.sh

--- start-nmap-exercise.sh
+++ start-nmap-exercise.sh
@@ -4,8 +4,8 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-if [ -f /usr/local/apache2/bin/apachectl ]
-   then /usr/local/apache2/bin/apachectl -f `pwd`/httpd.conf -k start 2>/dev/null &
+if [ -f start-apache.sh ]
+   then start-apache.sh
 else
    ncat -kl 80 2>/dev/null &
 fi
