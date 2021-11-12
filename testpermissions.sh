2123eeebca8c16e023be86533c9018867dd73249
rfifarek <rfifarek@gmail.com>
2021-11-11 23:27:59 -0800

    Update testpermissions.sh

--- testpermissions.sh
+++ testpermissions.sh
@@ -3,11 +3,11 @@
 if [[ -r "/etc/passwd" ]]; then
    echo /etc/passwd is readable
 else
-   echo Error: /etc/passwd is not readable
+   echo ERROR! /etc/passwd is not readable
 fi
 
 if [[ -r /etc/shadow ]]; then
-   echo Error: /etc/shadow is readable
+   echo ERROR! /etc/shadow is readable
 else
    echo /etc/shadow is not readable
 fi
