95f9788629126decc5aafbc16b634006b5f918ac
rfifarek <rfifarek@gmail.com>
2021-11-30 21:26:18 -0800

    Delete start-apache.sh

--- start-apache.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if [ "$EUID" -ne 0 ]
-  then echo "Error: Please run with sudo"
-  exit
-fi
-
-if [ -f /usr/local/apache2/bin/apachectl ]
-   then /usr/local/apache2/bin/apachectl -f `pwd`/httpd.conf -k start 2>/dev/null &
-fi
