e14885275ff602f8265158e0461b3103a643316c
rfifarek <rfifarek@gmail.com>
2021-12-08 00:02:30 -0800

    Update start-nmap-exercise.sh

--- start-nmap-exercise.sh
+++ start-nmap-exercise.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 if [ "$EUID" -ne 0 ]
-  then echo "Error: Please run with sudo"
+then 
+  echo "Error: Please run with sudo"
   exit
 fi
 
 if [ -f start-apache.sh ]
-   then start-apache.sh
+then 
+  start-apache.sh
+  /usr/sbin/ufw enable
+  /usr/sbin/ufw deny to 127.0.0.1 port 25
 else
    ncat -kl 80 2>/dev/null &
 fi
